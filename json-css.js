9d659c242b9ad7134ca507eb4d0a89ba1aecbc38
Auke van Slooten <auke@poefke.nl>
2017-05-29 16:00:35 +0200

    rewritten to more generic plugin structure

--- json-css.js
+++ json-css.js
@@ -1,122 +1,135 @@
-    editor.search = function() {
-        var renderData = function(node, value) {
-            var i;
-            if (typeof value === "string") {
-                node.setAttribute("value", value.toLowerCase());
-                node.setAttribute("type", "string");
-                node.simplyValue = value;
-            } else if (typeof value === "number") {
-                node.setAttribute("value", value);
-                node.setAttribute("type", "number");
-                node.simplyValue = value;
-            } else if (value === null) {
-                node.simplyValue = value;
-            } else if (typeof value === "object" && value.constructor === Array) {
-                for (i=0; i<value.length; i++) {
-                    node.appendChild(renderNode("entry", value[i]));
-                }
-            } else if (typeof value === "object") { 
-                for (i in value) {
-                    var newChild = renderNode(i, value[i]);
-                    node.appendChild(newChild);
-                }
-            } else {
-                console.log("Hier kan ik niks mee");
-                console.log(typeof value);
-                console.log(value);
-            }
-        };
+(function (root, factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        define(['buoy'], factory(root));
+    } else if ( typeof exports === 'object' ) {
+        module.exports = factory(require('buoy'));
+    } else {
+        root.myPlugin = factory(root, root.buoy);
+    }
+})(typeof global !== "undefined" ? global : this.window || this.global, function (root) {
 
-        var renderNode = function(key, value) {
-            var node;
-            if (key.match(/[^A-Za-z_-]/)) {
-                node = document.createElement("node");
-            } else {
-                node = document.createElement(key);
-            }
+    'use strict';
 
-            if (value === null) {
-                node.setAttribute("type", "null");
-            } else if ((typeof value === "object") && value.constructor === Array) {
-                node.setAttribute("type", "array");
-            } else {
-                node.setAttribute("type", "object");
-            }
+	var jsonCSS = {};
 
-            node.setAttribute("name", key);
-            renderData(node, value);
+
+    function renderData(node, value) {
+        var i;
+        if (typeof value === "string") {
+            node.setAttribute("value", value.toLowerCase());
+            node.setAttribute("type", "string");
             node.simplyValue = value;
-            return node;
-        };
-
-        var filterNodes = function(nodes, query) {
-            var result = [];
-            for (var i=0; i<nodes.length; i++) {
-                if (nodes[i].querySelector(query)) {
-                    result.push(nodes[i]);
-                }
+        } else if (typeof value === "number") {
+            node.setAttribute("value", value);
+            node.setAttribute("type", "number");
+            node.simplyValue = value;
+        } else if (value === null) {
+            node.simplyValue = value;
+        } else if (typeof value === "object" && value.constructor === Array) {
+            for (i=0; i<value.length; i++) {
+                node.appendChild(renderNode("entry", value[i]));
             }
-            return result;
-        };
-
-        var searchNodes = function(tree, queries) {
-            var baseQuery;
-
-            if (typeof queries === "string") {
-                baseQuery = queries;
-                queries = [];
-            } else {
-                baseQuery = queries[0];
+        } else if (typeof value === "object") { 
+            for (i in value) {
+                var newChild = renderNode(i, value[i]);
+                node.appendChild(newChild);
             }
-            var resultNodes = tree.querySelectorAll(baseQuery);
-            if (resultNodes.length === 1 && resultNodes[0].getAttribute("type") === "array") {
-                resultNodes = resultNodes[0].childNodes;
-            }
+        } else {
+            console.log("Hier kan ik niks mee");
+            console.log(typeof value);
+            console.log(value);
+        }
+    };
 
-            for (var i=1; i<queries.length; i++) {
-                resultNodes = filterNodes(resultNodes, queries[i]);
-            }
-            
-            var result = [];
-            for (i=0; i<resultNodes.length; i++) {
-                result.push({
-                    key : resultNodes[i].getAttribute('name'),
-                    value : resultNodes[i].simplyValue
-                });
-            }
-            return result;            
-        };
+    function renderNode(key, value) {
+        var node;
+        if (key.match(/[^A-Za-z_-]/)) {
+            node = document.createElement("node");
+        } else {
+            node = document.createElement(key);
+        }
 
-        return {
-            init : function(data) {
-                if (data.hasOwnProperty("search")) {
-                    return;
-                }
+        if (value === null) {
+            node.setAttribute("type", "null");
+        } else if ((typeof value === "object") && value.constructor === Array) {
+            node.setAttribute("type", "array");
+        } else {
+            node.setAttribute("type", "object");
+        }
 
-                Object.defineProperty(data, "search", {
-                    get : function() {
-                        return function() {
-                        //    console.time("search");
-                            if (!this.hasOwnProperty("searchElement")) {
-                                var searchElement = document.createElement("search");
-                                renderData(searchElement, this);
-                                Object.defineProperty(this, "searchElement", {
-                                    get : function() {
-                                        return searchElement;
-                                    },
-                                    enumerable : false,
-                                    configurable : false
-                                });
-                            }
-                            var result = searchNodes(this.searchElement, arguments);
-                        //    console.timeEnd("search");
-                            return result;
-                        }
-                    },
-                    enumerable : false,
-                    configurable : false
-                });
+        node.setAttribute("name", key);
+        renderData(node, value);
+        node.simplyValue = value;
+        return node;
+    };
+
+    function filterNodes(nodes, query) {
+        var result = [];
+        for (var i=0; i<nodes.length; i++) {
+            if (nodes[i].querySelector(query)) {
+                result.push(nodes[i]);
             }
         }
-    }();
-    
+        return result;
+    };
+
+    function searchNodes(tree, queries) {
+        var baseQuery;
+
+        if (typeof queries === "string") {
+            baseQuery = queries;
+            queries = [];
+        } else {
+            baseQuery = queries[0];
+        }
+        var resultNodes = tree.querySelectorAll(baseQuery);
+        if (resultNodes.length === 1 && resultNodes[0].getAttribute("type") === "array") {
+            resultNodes = resultNodes[0].childNodes;
+        }
+
+        for (var i=1; i<queries.length; i++) {
+            resultNodes = filterNodes(resultNodes, queries[i]);
+        }
+        
+        var result = [];
+        for (i=0; i<resultNodes.length; i++) {
+            result.push({
+                key : resultNodes[i].getAttribute('name'),
+                value : resultNodes[i].simplyValue
+            });
+        }
+        return result;            
+    };
+
+    jsonCSS.init = function(data) {
+        if (data.hasOwnProperty("search")) {
+            return;
+        }
+
+        Object.defineProperty(data, "search", {
+            get : function() {
+                return function() {
+                //    console.time("search");
+                    if (!this.hasOwnProperty("searchElement")) {
+                        var searchElement = document.createElement("search");
+                        renderData(searchElement, this);
+                        Object.defineProperty(this, "searchElement", {
+                            get : function() {
+                                return searchElement;
+                            },
+                            enumerable : false,
+                            configurable : false
+                        });
+                    }
+                    var result = searchNodes(this.searchElement, arguments);
+                //    console.timeEnd("search");
+                    return result;
+                }
+            },
+            enumerable : false,
+            configurable : false
+        });
+    }
+
+	return jsonCSS;
+});    
